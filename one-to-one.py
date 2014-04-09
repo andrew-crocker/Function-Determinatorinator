773033e81221905b3ae3360bf3f4ce0d225b1e27
Tommy Markley <tommylee2448@gmail.com>
2014-04-08 22:30:32 -0500

    Added Onto functionality

--- one-to-one.py
+++ one-to-one.py
@@ -1,7 +1,7 @@
 # one-to-one prompts the user for input and/or reads a list of function pairs from a file, then determines whether the data points represent a one-to-one function or not.
 
 def print_instructions():
-	print("\nThe following program is intended to take a list of function pairs as input, and it determines whether or not the function is one-to-one.\n")
+	print("\nThe following program is intended to take a list of function pairs as input, and it determines if the function is one-to-one and/or onto.\n")
 	print("You will be asked to enter function pairs either by hand, or by giving the name of a file where they are stored.\n")
 
 def multisplit(s, sep):
@@ -89,13 +89,66 @@ def is_OneToOne(codomain):
 				return 0
 	return 1
 
-def is_Onto(codomain):
+def is_Onto(Range, codomain):
+	for i in Range:
+		if i not in codomain:
+			return 0
+
 	return 1
 
-def process_input(domain, codomain):
+
+def ask_onto():
+	answer = input("Do you want to check if this function is Onto? [Yes/No]: ").lower()
+	if answer[0] == 'y':
+		return 1
+	else:
+		return 0
+
+def get_range():
+	location = input("Are the range values stored in a file? [Yes/No]: ").lower()
+	Range = []
+	while True:
+		try:
+			if location[0] != "y" and location[0] != "n":
+				second_prompt = 1
+				location = input("Try again.. input from file? [Y/N]: ").lower()
+			else:
+				break
+		except IndexError:
+			if not second_prompt:
+				location = input("Are the range values stored in a file? [Yes/No]: ").lower()
+			else:
+				location = input("Try again.. input from file? [Y/N]: ").lower()
+	
+	if location[0] == "y":
+		while True:
+			try:
+				filename = input("What file are the range values in? ")
+				infile = open(filename, "r")
+				break
+			except FileNotFoundError:
+				print("Looks like that file doesn't exist. Please try again.")
+		text_string = "".join(infile.readlines()).lower()
+		data = multisplit(text_string, "\n\t|'\".,/?><;:[]{}-_=+()!@#$%^&*`~ abcdefghijklmnopqrstuvwxyz")
+		for i in range(0, len(data)):
+			Range.append(data[i])
+
+	else:
+		data = input("Enter the range as a list of numbers and press Enter: ").lower()
+		data2 = multisplit(data, "\n\t|'\".,/?><;:[]{}-_=+()!@#$%^&*`~ abcdefghijklmnopqrstuvwxyz")
+		for i in range(0, len(data2)):
+			Range.append(data2[i])
+
+	return Range
+	
+
+def process_input(domain, codomain, Range):
 	if function_check(domain, codomain):
+		if Range:
+			Onto = is_Onto(Range, codomain)
+		else:
+			Onto = -1
 		OneToOne = is_OneToOne(codomain)
-		Onto = is_Onto(codomain)
 		return OneToOne, Onto, 1
 	else:
 		return 0, 0, 0
@@ -110,15 +163,18 @@ def print_result(OneToOne, Onto, Function):
 		else:
 			print("Not one-to-one.")
 		if Onto:
-			print();
-			# print("Onto not currently implemented.")
-		else:
+			print("Onto!")
+		elif Onto == 0:
 			print("Not onto.")
 
 def main():
 	print_instructions()
+	checkOnto = ask_onto()
+	Range = 0
+	if checkOnto:
+		Range = get_range()
 	domain, codomain = get_input()
-	OneToOne, Onto, Function = process_input(domain, codomain)
+	OneToOne, Onto, Function = process_input(domain, codomain, Range)
 	print_result(OneToOne, Onto, Function)
 
 main()
